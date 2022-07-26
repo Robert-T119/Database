cc2bfda8d1172e29a494ae82ad4dbf5f075f021f
Bohui <bohui@tbhs-mbp.home>
2022-07-26 11:06:34 +0100

    Second commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import storages.backends.s3boto3
@@ -204,7 +204,7 @@ STATIC_URL = '/static/'
     #os.path.join(BASE_DIR, "static"),
 #)
 
-# django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
