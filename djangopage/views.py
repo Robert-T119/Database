4af849b8cb039369d42d23728e961a9e08fbe983
Bohui <bohui@tbhs-mbp.home>
2022-08-04 15:10:58 +0100

    second commit

--- djangopage/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'djangopage/home.html')
-
-# blog->templates ->blog ->template.html  (Logic about creating a template and blog folder(django convention))
