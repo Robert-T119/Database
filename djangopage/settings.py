6364fe2ac4344fbb5a078f6d2a027e4f9c2b8c18
Bohui <bohui@tbhs-mbp.home>
2022-08-26 17:10:40 +0100

    second commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '244e123eb364ed5e4f4ffc0acc3084da43c951036847108'
+SECRET_KEY = '244e123eb364ed5e4f4ffc0acc3084da43c951036847108'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
