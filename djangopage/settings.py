bb8856227d0bc94ae368f0a3c8ff21c753997ea4
Bohui <bohui@tbhs-mbp.home>
2022-08-26 19:58:57 +0100

    second commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -105,15 +105,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES = {
+    },
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:8000/database', conn_max_age=600 )
+        default='postgresql://postgres:postgres@localhost:8000/database', conn_max_age=600)
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
