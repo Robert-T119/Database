f35012d14d2bf0d8a7a3a18a543df2aa3b38ee68
Bohui <bohui@tbhs-mbp.home>
2022-08-26 21:01:58 +0100

    second commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -105,10 +105,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://USER:8avuYWXy2WNPlWzL9PYNcAGNWobHi3Fb@127.0.0.1:5432/sdswefd2mlkksdf', conn_max_age=600)
+    }
 }
 
 # Password validation
