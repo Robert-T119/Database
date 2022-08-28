60f3ca182b14dbf492e10e9be6e4de582158e7bf
Bohui <bohui@tbhs-mbp.home>
2022-08-28 16:39:44 +0100

    fourth commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'djangopage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nickelpure',
+        'USER': 'postgres',
+        'PASSWORD': 'tangbohui',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- nickelpure/models.py
+++ nickelpure/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
