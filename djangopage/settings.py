c08d14bb9c2704b15f0b2db66c80989798730c1e
Bohui <bohui@tbhs-mbp.home>
2022-08-28 19:46:25 +0100

    second commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -103,14 +103,12 @@ WSGI_APPLICATION = 'djangopage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangopage',
-        'USER': 'postgres',
-        'PASSWORD': 'tangbohui',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
