f049e167aa30634a93f97e17766c84b879118165
Bohui <bohui@tbhs-mbp.home>
2022-08-29 18:03:33 +0100

    fifth commit

--- djangopage/settings.py
+++ djangopage/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = 'djangopage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'zj14rANRvsBaWi3d6fNV',
+        'HOST': 'containers-us-west-84.railway.app',
+        'PORT': '6824',
     }
 }
 
